409278ed8cd6457f7e60577019e7f393e0ed0cab
henriquericcio <henrique.riccio@iconware.com.br>
2012-12-12 18:02:28 -0200

    Prot: refactoring

---
+++ src/EasyArchitecture/Instances/Cache.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace EasyArchitecture.Instances
+{
+    public class Cache
+    {
+//                private static readonly ICacheManager CacheInstance = CacheFactory.GetCacheManager();
+
+        public void Add(string key, object obj)
+        {
+            //CacheInstance.Add(key,obj);
+        }
+
+        public void Add(string key, object obj, TimeSpan expiration)
+        {
+            //CacheInstance.Add(key, obj, CacheItemPriority.Normal, null, new SlidingTime(expiration));
+        }
+
+        public void Remove(string key)
+        {
+            //CacheInstance.Remove(key);
+        }
+
+        public void Flush()
+        {
+            //CacheInstance.Flush();
+        }
+
+        public object GetData(string key)
+        {
+            //return CacheInstance.GetData(key);
+            return null;
+        }
+
+        public T GetData<T>(string key)
+        {
+            //return (T)CacheInstance.GetData(key);
+            return default(T);
+        }
+
+        public bool Contains(string key)
+        {
+            //return CacheInstance.Contains(key);
+            return false;
+        }
+    }
+    
+}
--- src/EasyArchitecture/Mechanisms/Cache.cs
+++ src/EasyArchitecture/Mechanisms/Cache.cs
@@ -7,5 +7,43 @@ namespace EasyArchitecture.Mechanisms
 {
     public class Cache
     {
+        //private static readonly ICacheManager CacheInstance = CacheFactory.GetCacheManager();
+        private static readonly EasyArchitecture.Instances.Cache CacheInstance = new EasyArchitecture.Instances.Cache();
+
+        public static void Add(string key, object obj)
+        {
+            CacheInstance.Add(key, obj);
+        }
+
+        public static void Add(string key, object obj, TimeSpan expiration)
+        {
+            //CacheInstance.Add(key, obj, CacheItemPriority.Normal, null, new SlidingTime(expiration));
+        }
+
+        public static void Remove(string key)
+        {
+            CacheInstance.Remove(key);
+        }
+
+        public static void Flush()
+        {
+            CacheInstance.Flush();
+        }
+
+        public static object GetData(string key)
+        {
+            return CacheInstance.GetData(key);
+        }
+
+        public static T GetData<T>(string key)
+        {
+            return (T)CacheInstance.GetData(key);
+        }
+
+        public static bool Contains(string key)
+        {
+            return CacheInstance.Contains(key);
+        }
     }
 }
+
