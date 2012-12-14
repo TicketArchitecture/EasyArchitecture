b238a84ff93f971031c7485d83c3e9f9c4f6b4a2
henriquericcio <henrique.riccio@iconware.com.br>
2012-12-14 13:45:16 -0200

    Prot: refactoring

--- src/EasyArchitecture/IoC/Plugin/BultIn/InjectionPlugin.cs
+++ src/EasyArchitecture/IoC/Plugin/BultIn/InjectionPlugin.cs
@@ -1,26 +1,99 @@
 ﻿using System;
+using System.Collections.Generic;
 using EasyArchitecture.IoC.Plugin.Contracts;
 
 namespace EasyArchitecture.IoC.Plugin.BultIn
 {
     internal class IocPlugin : IIoCPlugin
     {
-        private readonly Container _container = new Container();
+        private readonly Dictionary<Type, Type> _registeredTypes = new Dictionary<Type, Type>();
 
         public void Register<T, T1>() where T1 : T
         {
-            //container.Register<T,T1>();
-            _container.RegisterType(typeof(T), typeof(T1));
+            RegisterType(typeof (T), typeof (T1));
         }
 
         public T Resolve<T>()
         {
-            return _container.Resolve<T>();
+            return (T) Resolve(typeof (T));
         }
 
         public void Register(Type interfaceType, Type implementationType, bool useInterception)
         {
-            _container.RegisterType(interfaceType,implementationType);
+            RegisterType(interfaceType, implementationType);
+        }
+
+        private object Resolve(Type interfaceType)
+        {
+            /*-----------------
+             * RESOLVE T -> T1
+             * ----------------*/
+            //find impl
+            //TODO: create a NotRegisteredException
+            if (!_registeredTypes.ContainsKey(interfaceType))
+                throw new ArgumentException("Type not registered", interfaceType.ToString());
+            //resolve
+            var implementationType = _registeredTypes[interfaceType];
+
+
+            /*-------------
+             * BUILD
+             * ------------*/
+
+            //todo trabalho para devolver esse kra preenchido
+            object instance = null;
+
+            //-----------------------caminho feliz
+            //ha 1 constructor default (parameterless)
+            var constructors = implementationType.GetConstructors();
+
+            //e se nao tiver nenhum constructor? //exception --> cannot build
+
+            var parameterlessConstructor = Array.Find(constructors, c => c.GetParameters().Length == 0);
+            if (parameterlessConstructor != null)
+            {
+                //instance = implementationType.Assembly.CreateInstance(implementationType.FullName);
+                instance = Activator.CreateInstance(implementationType);
+            }
+            else
+            {
+                //-----------------------caminho triste
+                //ha 1 constructor com parametros
+
+                var constructor = constructors[0];
+                //var constructorParameters = constructor.GetParameters();
+                var objects = new List<object>();
+                //foreach parameter -> call recursivelly
+                foreach (var constructorParameter in constructor.GetParameters())
+                {
+                    //resolver parametros
+
+                    objects.Add(this.Resolve(constructorParameter.ParameterType));
+                }
+
+                //instance = implementationType.Assembly.CreateInstance(implementationType.FullName,);
+                instance = Activator.CreateInstance(implementationType, objects.ToArray());
+            }
+
+
+            //se for marcado interception, gerar proxy
+            //var proxy = ProxyFactory.NewInstance(instance);
+
+            //devolver
+            return instance;
+        }
+
+        private void RegisterType(Type interfaceType, Type implementationType)
+        {
+            if (_registeredTypes.ContainsKey(interfaceType))
+            {
+                _registeredTypes[interfaceType] = implementationType;
+            }
+            else
+            {
+                _registeredTypes.Add(interfaceType, implementationType);
+            }
         }
     }
+
 }
