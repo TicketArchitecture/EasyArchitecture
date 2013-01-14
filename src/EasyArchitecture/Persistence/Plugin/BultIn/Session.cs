49918f3ccd476714512608719493fe711ae8e5b3
alexxxander <alexander.molinari@gmail.com>
2013-01-14 00:50:42 -0200

    Improving comments. Anthough Get<T>(object qbe) it's working, it's not finished yet.

--- src/EasyArchitecture/Persistence/Plugin/BultIn/Session.cs
+++ src/EasyArchitecture/Persistence/Plugin/BultIn/Session.cs
@@ -104,11 +104,10 @@ namespace EasyArchitecture.Persistence.Plugin.BultIn
         }
 
         /// <summary>
-        /// Retrieves all existent objects according to the given example.
-        /// </summary>
-        /// <remarks>Not initialized primitive properties as well as null references are ignored.</remarks>
+        /// Retrieves all existent objects according to the given example in <paramref name="qbe"/>. 
+        /// <para>Not initialized primitive properties as well as boolean and null references are ignored in the comparison.</para></summary>
         /// <typeparam name="T">Type of the given example</typeparam>
-        /// <param name="qbe">Query By Example object</param>
+        /// <param name="qbe">object used in query by example <see cref="http://en.wikipedia.org/wiki/Query_by_Example"/></param>
         /// <returns>A list containing objects found according to the <paramref name="qbe"/>, if any.</returns>
         public IList<T> Get<T>(object qbe)
         {
@@ -127,50 +126,34 @@ namespace EasyArchitecture.Persistence.Plugin.BultIn
             //}
 
             
-            ArrayList list = null;
+            ArrayList specificTypeList = null;
             if (_dataBase.ContainsKey(typeof(T)))
-                list = _dataBase[typeof(T)];
+                specificTypeList = _dataBase[typeof(T)];
 
-            var listlistlist = new ArrayList();
-
-
-           // var dictionary = new Dictionary<string, object>();
-
-
-//obtem a lista de propriedades/valores, q devem ser usados na comparacao
+            var matchedItemsList = new List<T>();
             var type = typeof (T);
             var properties = type.GetProperties();
-            //foreach (var propertyInfo in properties)
-            //{
-            //    var name = propertyInfo.Name;
-            //    var value = propertyInfo.GetValue(key);
 
-            //    if(value!=null)
-            //    dictionary.Add(name,value);
-            //}
-
-
-            //compara
-            foreach (var item in list)
+            foreach (var item in specificTypeList)
             {
-                //compara o item
                 if(exampleMatchesItem(item,properties,qbe))
                 {
-                    listlistlist.Add(item);
+                    matchedItemsList.Add((T)item);
                 }
             }
 
-            //if (list != null) return new List<T>((IEnumerable<T>) list);
-            var listlistlistlist = new List<T>();
-            foreach (var item in listlistlist)
-            {
-                listlistlistlist.Add((T)item);
-                
-            }
 
-            return listlistlistlist;
+            return matchedItemsList;
         }
 
+
+        /// <summary>
+        /// Compares <paramref name="qbe"/> against <paramref name="item"/> 
+        /// </summary>
+        /// <param name="item">The instance to compare to</param>
+        /// <param name="typeProperties">Properties of the <paramref name="qbe"/> and <paramref name="item"/> reference Type.</param>
+        /// <param name="qbe">The Query by example object to compare from</param>
+        /// <returns>true when all <paramref name="qbe"/> properties' values are equal to item's properties. False otherwise.</returns>
         private bool exampleMatchesItem(object item, PropertyInfo[] typeProperties, object qbe)
         {
             var ok = false;
@@ -180,7 +163,7 @@ namespace EasyArchitecture.Persistence.Plugin.BultIn
                 
                 var qbeValue = property.GetValue(qbe, BindingFlags.Default, null, null, null);
 
-                //ignore all these non initialized properties
+                //ignore all these non initialized properties, as well as boolean ones
                 if(qbeValue == null || qbeValue is bool || qbeValue.Equals(0) || qbeValue.Equals(""))
                     continue;
 
