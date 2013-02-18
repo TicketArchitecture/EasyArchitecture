9ff5eba900441253acefe7d7bc7749a3db8594aa
henriquericcio <henrique.riccio@iconware.com.br>
2013-02-18 18:36:37 -0300

    [CleanUp] Copyright

--- tests/EasyArchitecture.Tests/Properties/AssemblyInfo.cs
+++ tests/EasyArchitecture.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EasyArchitecture.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright (c) 2010 Henrique Riccio")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
