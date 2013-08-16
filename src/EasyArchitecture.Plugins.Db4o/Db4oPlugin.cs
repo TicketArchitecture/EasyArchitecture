4dae4231cde5c3f6ec6759bcd916d37fc1d1f078
Henrique Riccio <henrique.riccio@iconware.com.br>
2013-08-16 16:22:12 -0300

    [Persistence] Db4o optional path

--- src/EasyArchitecture.Plugins.Db4o/Db4oPlugin.cs
+++ src/EasyArchitecture.Plugins.Db4o/Db4oPlugin.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Configuration;
+using System.IO;
 using EasyArchitecture.Plugins.Contracts.Persistence;
 
 namespace EasyArchitecture.Plugins.Db4o
@@ -15,8 +17,11 @@ namespace EasyArchitecture.Plugins.Db4o
 
         protected override void ConfigurePlugin(PluginConfiguration pluginConfiguration, PluginInspector pluginInspector)
         {
-            var file = string.Format("db{0}.db", pluginConfiguration.ModuleName);
-            _path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, file);
+            var key = String.Format("{0}.Db4oPlugin.DbFileLocation", pluginConfiguration.ModuleName);
+            var value = ConfigurationManager.AppSettings[key];
+
+            _path = value ??
+            Path.Combine(AppDomain.CurrentDomain.BaseDirectory, string.Format("db{0}.db", pluginConfiguration.ModuleName));
         }
     }
 }
