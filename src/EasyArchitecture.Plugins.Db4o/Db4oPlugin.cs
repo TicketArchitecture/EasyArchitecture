23aff43ae1ce6ced12c6aec30f5eff65063d0887
Henrique Riccio <henrique.riccio@iconware.com.br>
2013-08-15 21:50:26 -0300

    [Persistence] Added Db4o

---
+++ src/EasyArchitecture.Plugins.Db4o/Db4oPlugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Db4objects.Db4o;
+using EasyArchitecture.Core;
+using EasyArchitecture.Plugins.Contracts.Persistence;
+
+namespace EasyArchitecture.Plugins.Db4o
+{
+    public class Db4oPlugin: Plugin, IPersistencePlugin
+    {
+
+        private IObjectContainer db ;
+
+        public IPersistence GetInstance()
+        {
+            return new Db4oPersistence(db);
+        }
+
+        protected override void ConfigurePlugin(PluginConfiguration pluginConfiguration, PluginInspector pluginInspector)
+        {
+            var assembly = pluginConfiguration.InfrastructureAssembly;
+            var file = string.Format("\\db{0}.db", pluginConfiguration.ModuleName);
+
+            db = Db4oFactory.OpenFile(AppDomain.CurrentDomain.BaseDirectory + file);
+        }
+    }
+}
---
+++ tests/EasyArchitecture.Plugins.Db4o.Tests/PersistenceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using EasyArchitecture.Plugins.Tests.Persistence;
+using NUnit.Framework;
+
+namespace EasyArchitecture.Plugins.Db4o.Tests
+{
+    [TestFixture]
+    public class PersistenceTest:MinimalPersistenceTest
+    {
+        [SetUp]
+        public override void SetUp()
+        {
+            var plugin = new Db4oPlugin();
+            var moduleName = Guid.NewGuid().ToString();
+
+            PluginInspector pluginInspector;
+            plugin.Configure(new PluginConfiguration(moduleName, null, null, Assembly.GetExecutingAssembly()),
+                             out pluginInspector);
+
+            PluginInstance = plugin.GetInstance();
+        }
+    }
+}
